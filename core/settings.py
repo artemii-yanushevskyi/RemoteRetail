59aa775b512fcb64c416ee4230610fcfa830ce85
Artemii Yanushevskyi <artemii.yanushevskyi@gmail.com>
2018-10-26 20:30:42 +0300

    hello RemoteRetail page

--- core/settings.py
+++ core/settings.py
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+INSTALLED_APPS.append('retail')
+TIME_ZONE = 'Europe/Kiev'
--- retail/views.py
+++ retail/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+# from .models import ModelName
+
+# Create your views here.
+
+def landing_page(request):
+    return render(request, 'retail/index.html')
