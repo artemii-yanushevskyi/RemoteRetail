c764f53756524be61edc830d833104b17cdd4e77
Artemii Yanushevskyi <artemii.yanushevskyi@gmail.com>
2018-10-26 22:15:32 +0300

    Add models

--- retail/models.py
+++ retail/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Patron(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.CharField(max_length=20, null=True)
+    balance = models.IntegerField(default=0)
+
+
+class Purchase(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    time = models.DateTimeField(default=timezone.now)
+    seller = models.ForeignKey(Patron, on_delete=models.CASCADE, related_name='seller')
+    buyer = models.ForeignKey(Patron, on_delete=models.CASCADE, related_name='buyer')
+
+
